e2279b05a12796d14dc2c78ea15959a8a4ae5fe1
kuabrs <95240062+kuabrs@users.noreply.github.com>
2023-04-24 13:13:21 -0300

    Add files via upload

---
+++ WebApp/Context/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApp.Models;
+
+namespace WebApp.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("WebAppDB") { }
+
+        public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
---
+++ WebApp/Controllers/FabricantesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Context;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private readonly EFContext context = new EFContext();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(f => f.Nome));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Fabricantes.Add(fabricante);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+    }
+}
