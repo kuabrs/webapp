a337d0d048fc11526e0e1fd7dfeec00ce70a10c5
KAUÃ BATISTA RIBEIRO DE SOUZA <kaua.batista@academico.ifrn.edu.br>
2023-05-29 14:17:37 -0300

    começo capítulo 4

--- WebApp/Context/EFContext.cs
+++ WebApp/Context/EFContext.cs
@@ -7,11 +7,14 @@ using WebApp.Models;
 
 namespace WebApp.Context
 {
-    public class EFContext : DbContext
-    {
-        public EFContext() : base("WebAppDB") { }
-
-        public DbSet<Fabricante> Fabricantes { get; set; }
-        public DbSet<Categoria> Categorias { get; set; }
-    }
+public class EFContext : DbContext
+{
+public EFContext() : base("WebAppDB") {
+Database.SetInitializer<EFContext>(
+new DropCreateDatabaseIfModelChanges<EFContext>());
+}
+public DbSet<Categoria> Categorias { get; set; }
+public DbSet<Fabricante> Fabricantes { get; set; }
+public DbSet<Produto> Produtos { get; set; }
+}
 }
--- WebApp/Models/Fabricante.cs
+++ WebApp/Models/Fabricante.cs
@@ -7,7 +7,10 @@ namespace WebApp.Models
 {
     public class Fabricante
     {
+
         public long FabricanteId { get; set; }
         public string Nome { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; }
+
     }
 }
