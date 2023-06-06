ce8db956daf2ceb135989d623e5b94973a7bc5b9
KAUÃ BATISTA RIBEIRO DE SOUZA <kaua.batista@academico.ifrn.edu.br>
2023-06-06 15:59:09 +0000

    atividade cap 4

--- WebApp/Controllers/CategoriasController.cs
+++ WebApp/Controllers/CategoriasController.cs
@@ -68,7 +68,8 @@ namespace WebApp.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Categoria categoria = context.Categorias.Find(id);
+            Categoria categoria = context.Categorias.Where(c => c.CategoriaId == id).
+            Include("Produtos.Fabricante").First();
             if (categoria == null)
             {
                 return HttpNotFound();
