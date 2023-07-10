7fcb6ef9183f3977dd8262e0bb9dbb24aecfe8b4
KAUÃ BATISTA RIBEIRO DE SOUZA <kaua.batista@academico.ifrn.edu.br>
2023-07-10 14:26:15 -0300

    camada de serviço

---
+++ Servicos/Tabelas/CategoriaServico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.Tabelas;
+using Persistencia.DAL.Tabelas;
+
+namespace Servico.Tabelas
+{
+    public class CategoriaServico
+    {
+        private CategoriaDAL categoriaDAL = new CategoriaDAL();
+        public IQueryable<Categoria> ObterCategoriasClassificadasPorNome()
+        {
+            return categoriaDAL.ObterCategoriasClassificadasPorNome();
+        }
+    }
+}
