3ece9819a4941153ddc49851255094e4fd6e0976
KAUÃ BATISTA RIBEIRO DE SOUZA <kaua.batista@academico.ifrn.edu.br>
2023-09-26 15:51:33 -0300

    inicio autenticação

---
+++ WebApp/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public IdentityDbContextAplicacao() : base("IdentityDB")
+        { }
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>(new IdentityDbInit());
+        }
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();
+        }
+
+        public System.Data.Entity.DbSet<WebAppProjeto2023.Areas.Seguranca.Data.Usuario> Usuarios { get; set; }
+    }
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<IdentityDbContextAplicacao> { }
+}
---
+++ WebApp/Infraestrutura/Usuario.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApp.Infraestrutura
+{
+    public class Usuario
+    {
+    }
+}
