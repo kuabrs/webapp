a0442aa9ab246e0ab856048dd3f6e6708edde04a
KAUÃ BATISTA RIBEIRO DE SOUZA <kaua.batista@academico.ifrn.edu.br>
2023-10-02 14:11:19 -0300

    começo admin controller e index

---
+++ WebApp/Areas/Seguranca/Controllers/AdminController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Infraestrutura;
+
+namespace WebApp.Areas.Seguranca.Controllers
+{
+    public class AdminController : Controller
+    {
+        // Definição da Propriedade GerenciadorUsuario
+        private GerenciadorUsuario GerenciadorUsuario
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().
+                GetUserManager<GerenciadorUsuario>();
+
+            }
+        }
+        // GET: Seguranca/Admin
+        public ActionResult Index()
+        {
+            return View(GerenciadorUsuario.Users);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+        [HttpPost]
+        public ActionResult Create(UsuarioViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuario user = new Usuario
+                {
+                    UserName = model.Nome,
+                    Email = model.Email
+                };
+                IdentityResult result = GerenciadorUsuario.Create(user, model.Senha);
+                if (result.Succeeded)
+                { return RedirectToAction("Index"); }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(model);
+        }
+    }
+
+}
+    }
+}
